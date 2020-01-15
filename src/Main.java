22a9f1f68af46c9a4671f14d41b3b940abf583c1
Krzysztof Swiatly <swiatlyk@gmail.com>
2020-01-15 19:21:04 +0100

    Added filling out the array.

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,17 @@ public class Main {
         // write your code here
         System.out.println("One-dimensional array");
 
-        int[] array = new int[5];
-        int[] array2 = {4, 44, 444};
-        array[1] = 20;
-        System.out.println(array[0] + " " + array[1]);
+        //int[] array = new int[5];
+        int[] array2 = new int [10];
+        //array[1] = 20;
+        //System.out.println(array[0] + " " + array[1]);
         System.out.println("Amount of elements:" + " " + array2.length);
+        //Adding to array
+        for (int i = 0; i < 10; i++) {
+            array2[i] = i +1 ;
+        }
+        for (int x : array2) {
+            System.out.println(x);
+        }
     }
 }
