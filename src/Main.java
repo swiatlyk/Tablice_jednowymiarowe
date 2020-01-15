00790fc2baf8601af6ce1bd4fff891bde36a9b84
Krzysztof Swiatly <swiatlyk@gmail.com>
2020-01-15 18:25:39 +0100

    Initial commit: First release.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Tablice jednowymiarowe");
+
+        int[] tablica = new int[5];
+        int[] tablica2 = {4, 44, 444};
+        tablica[1] = 20;
+        System.out.println(tablica[0] + " " + tablica[1]);
+        System.out.println("Ilosc elementow w tablicy:" + " " + tablica2.length);
+    }
+}
